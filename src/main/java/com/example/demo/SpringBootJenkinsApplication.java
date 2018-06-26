cc7ec4918584f258ecd6576a9e0482ca04f4aa5f
heShaoWen <heShaoWenCode@163.com>
2018-06-26 20:54:45 +0800

    新增

--- src/main/java/com/example/demo/SpringBootJenkinsApplication.java
+++ src/main/java/com/example/demo/SpringBootJenkinsApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootJenkinsApplication {
 
 	public static void main(String[] args) {
+		
+		
 		SpringApplication.run(SpringBootJenkinsApplication.class, args);
 	}
 }
